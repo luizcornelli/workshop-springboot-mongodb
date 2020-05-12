be1a8dd8503efe6786fe764a8eb08cc185fcdd4b
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-05-12 16:53:27 -0300

    Entity User funcionando

--- src/main/java/com/workshopmongo/resources/UserResource.java
+++ src/main/java/com/workshopmongo/resources/UserResource.java
@@ -1,16 +1,29 @@
 package com.workshopmongo.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.workshopmongo.domain.User;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public ResponseEntity<List<User>> listar() {
 		
-		return "REST está funcionando"; 
+		User maria = new User("1", "Maria", "maria@gmail.com"); 		
+		User alex = new User("2", "Alex", "alex@gmail.com");
+		
+		List<User> list = new ArrayList<>(); 
+		list.addAll(Arrays.asList(maria, alex));
+		
+		return ResponseEntity.ok().body(list); 
 	}
 }
