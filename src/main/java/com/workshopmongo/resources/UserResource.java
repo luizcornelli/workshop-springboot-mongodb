104857e8a16c4d10928b86576b511a88ccce38da
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-05-12 16:30:36 -0300

    Rest testado

---
+++ src/main/java/com/workshopmongo/resources/UserResource.java
@@ -0,0 +1,16 @@
+package com.workshopmongo.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		
+		return "REST está funcionando"; 
+	}
+}
