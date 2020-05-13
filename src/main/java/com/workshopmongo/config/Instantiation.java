d37fda64ed82167218c6eba47df8174095df34ae
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-05-13 10:28:22 -0300

    Referenciando posts do User

--- src/main/java/com/workshopmongo/config/Instantiation.java
+++ src/main/java/com/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.workshopmongo.domain.Post;
 import com.workshopmongo.domain.User;
+import com.workshopmongo.dto.AuthorDTO;
 import com.workshopmongo.repository.PostRepository;
 import com.workshopmongo.repository.UserRepository;
 
@@ -35,13 +36,21 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex", "alex@gmail.com"); 
 		User jose = new User(null, "José", "jose@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/09/2019"), "Viagem para São Paulo", "Uhul cheguei em São Paulo!", maria); 
-		Post post2 = new Post(null, sdf.parse("23/10/2020"), "Almoçando no Shopping", "Saudades de comer esse Burguer King", alex); 
-		Post post3 = new Post(null, sdf.parse("23/10/2020"), "Na Praia", "Tomando banho de mar coisa maravilhosa", jose);
-		Post post4 = new Post(null, sdf.parse("13/05/2020"), "Na quarentena", "Não aguento mais essa quarentena, partiu sair!!!", jose);
+		userRepository.saveAll(Arrays.asList(maria, alex, jose));
+		
+		Post post1 = new Post(null, sdf.parse("21/09/2019"), "Viagem para São Paulo", "Uhul cheguei em São Paulo!", new AuthorDTO(maria)); 
+		Post post2 = new Post(null, sdf.parse("23/10/2020"), "Almoçando no Shopping", "Saudades de comer esse Burguer King", new AuthorDTO(alex)); 
+		Post post3 = new Post(null, sdf.parse("23/10/2020"), "Na Praia", "Tomando banho de mar coisa maravilhosa", new AuthorDTO(jose));
+		Post post4 = new Post(null, sdf.parse("13/05/2020"), "Na quarentena", "Não aguento mais essa quarentena, partiu sair!!!", new AuthorDTO(jose));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2, post3, post4)); 
+		
+		maria.getPosts().addAll(Arrays.asList(post1)); 
+		alex.getPosts().addAll(Arrays.asList(post2));
+		jose.getPosts().addAll(Arrays.asList(post3, post4)); 
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, jose));
-		postRepository.saveAll(Arrays.asList(post1, post2, post3, post4)); 
+		
 	}
 
 }
